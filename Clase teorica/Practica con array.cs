c38db64a89564496c119dc3b2b0c8b60482abed3
Daniel Castillo <ec1335784@gmail.com>
2021-08-13 09:25:45 -0600

    Agregar archivos de proyecto.

---
+++ Clase teorica/Practica con array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_teorica
+{
+    class Practica_con_array
+    {
+        static void Main()
+        {
+
+            
+            string[] estudiantes = { "Diego", "Pedro", "Harolina", "Dariana", "Hugo", "Jennifer", "Maria", "Sonia", "Kenia", "Francisco" };
+
+            int length = estudiantes.Length;
+
+            //Ordena el array de manera ascendente
+            Array.Sort(estudiantes);
+
+            
+            /* muestra la solución
+             * ya de manera ordenada
+             */
+            Console.WriteLine("Array ordenado:\n");
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(estudiantes[i]);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Clase teorica/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_teorica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> EmployeeList = new Dictionary<string, string>();
+
+            EmployeeList.Add("Carlos Mauricio", "Programador");
+            EmployeeList.Add("Jorge Castro", "Gerente");
+            EmployeeList.Add("Juan Guarnizo", "Ingeniero");
+
+            Dictionary<string, int> Students = new Dictionary<string, int>();
+
+            Students.Add("Carlos Mauricio", 15);
+            Students.Add("Jorge Castro", 40);
+            Students.Add("Juan Guarnizo", 38);
+
+           
+            foreach (KeyValuePair<string, string> employee in EmployeeList)
+            {
+                Console.WriteLine("Clave: {0}, Valor: {1}", employee.Key, employee.Value);
+            }
+
+          
+            var dic = EmployeeList.Keys.ToList();
+            dic.Sort();
+
+            Console.WriteLine("Diccionario ordenado");
+            foreach (var item in dic)
+            {
+                Console.WriteLine("{0}: {1}", item, EmployeeList[item]);
+            }
+
+         
+            Console.WriteLine("Elementos en el diccionario: {0}", EmployeeList.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
